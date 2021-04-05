16122fe6a36d2e4ecb12ba4e13f2478614abc729
Aris Katopodis <ariskatopodis1@gmail.com>
2021-04-05 20:48:21 +0300

    made coingeckoapi to return promises

--- back-end/src/utils/coingeckoApi.js
+++ back-end/src/utils/coingeckoApi.js
@@ -1,44 +1,57 @@
 const request = require("request");
 
 const apiUrl = "https://api.coingecko.com/api/v3";
-const coinList = (cb) =>{
-    const url = apiUrl + "/coins/list";
-    request({ url: url, json: true }, (error, res) => {
-        if (error) {
-            cb("Unable to connect to coingecko api service!",undefined);
-        }
-        else {
-            const data = res.body;
-            cb(undefined,data)
-        }
-    });
-}
-
-const pingApi = (cb) =>{
-        const url = apiUrl + "/ping";
+const coinList = () =>{
+    return new Promise((resolve,reject)=>{
+        const url = apiUrl + "/coins/list";
         request({ url: url, json: true }, (error, res) => {
             if (error) {
-                cb("Unable to connect to coingecko api service!",undefined);
+                reject("Unable to connect to coingecko api service!");
             }
             else {
                 const data = res.body;
-                cb(undefined,data)
+                resolve(data)
             }
         });
+    })
+    
+    
 }
 
-const priceOfCoins = (coins ,currency, cb) =>{
+const pingApi = () =>{
+    return new Promise((resolve,reject)=>{
+        const url = apiUrl + "/ping";
+        request({ url: url, json: true }, (error, res) => {
+            if (error) {
+                // cb("Unable to connect to coingecko api service!",undefined);
+                reject("Unable to connect to coingecko api service!");
+            }
+            try {
+                const data = res.body;
+                // cb(undefined,data)
+                resolve(data);
+            }catch(e){
+                reject("Unable to connect to coingecko api service!");
+            }
+        });
+    })
+       
+}
+
+const priceOfCoins = (coins ,currency) =>{
+    return new Promise((resolve,reject)=>{
+        const url = apiUrl + "/simple/price?ids="+coins.join('%2C')+"&vs_currencies="+currency.join('%2C')+"&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true";
+        request({ url: url, json: true }, (error, res) => {
+            if (error) {
+                reject("Unable to connect to coingecko api service!");
+            }
+            else {
+                const data = res.body;
+                resolve(data)
+            }
+        });
+    })
     
-    const url = apiUrl + "/simple/price?ids="+coins.join('%2C')+"&vs_currencies="+currency.join('%2C')+"&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true";
-    request({ url: url, json: true }, (error, res) => {
-        if (error) {
-            cb("Unable to connect to coingecko api service!",undefined);
-        }
-        else {
-            const data = res.body;
-            cb(undefined,data)
-        }
-    });
 }
 
 
--- back-end/src/utils/coingeckoPlayground.js
+++ back-end/src/utils/coingeckoPlayground.js
@@ -1,11 +1,16 @@
 const  {pingApi , coinList, priceOfCoins }= require("./coingeckoApi");
-
-
-    priceOfCoins(["bitcoin","ethereum"],["usd","eur"],(error,res)=>{
-        if(!error){
-            console.log(res)
-        }else{
-            console.log("A")
+    //promise
+    const a = async() => {
+        try{
+            const b = await pingApi();
+            const c = await coinList();
+            const d = await priceOfCoins(["bitcoin","ethereum"],["usd","eur"]);
+            console.log(b)
+            console.log(c);
+            console.log(d);     
+        }catch(e) {
+            console.log(e)
         }
-    })
+    }
 
+     a();
