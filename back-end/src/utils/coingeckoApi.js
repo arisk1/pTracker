c3c2eba5e9c17b389741b2d01dfc6edaf03ce067
Aris Katopodis <ariskatopodis1@gmail.com>
2021-04-09 18:55:43 +0300

    Created Porfolio schema , Created Portfolio router-add,remove coins- , Created a sample homepage router , Created utils folder for coingecko api , Tried out coingecko api and created reusable functions with it ,  Additional tests for user and portfolios

--- back-end/src/utils/coingeckoApi.js
+++ back-end/src/utils/coingeckoApi.js
@@ -1,73 +1,40 @@
-const request = require("request");
+const axios = require('axios');
 
 const apiUrl = "https://api.coingecko.com/api/v3";
 
-const coinList = () =>{
-    return new Promise((resolve,reject)=>{
-        const url = apiUrl + "/coins/list";
-        request({ url: url, json: true }, (error, res) => {
-            if (error) {
-                reject("Unable to connect to coingecko api service!");
-            }
-            else {
-                const data = res.body;
-                resolve(data)
-            }
-        });
-    })  
+const coinList =async () => {
+    const url = apiUrl + "/coins/list";
+    try {
+        return await axios.get(url)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
-const coinListMarkets = (currency,order) =>{
-    return new Promise((resolve,reject)=>{
-        //default 
-        //vs_currency=usd 
-        //order=market_cap_desc
-        const url = apiUrl + "/coins/markets?vs_currency="+ (currency ? currency : "usd")+"&order="+ (order ? order : "market_cap_desc") +"market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C14d";
+const coinListMarkets = async (currency, order) => {
+    const url = apiUrl + "/coins/markets?vs_currency=" + (currency ? currency : "usd") + "&order=" + (order ? order : "market_cap_desc") + "market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C14d";
+    try {
+        return await axios.get(url);
         
-        request({ url: url, json: true }, (error, res) => {
-            if (error) {
-                reject("Unable to connect to coingecko api service!");
-            }
-            else {
-                const data = res.body;
-                resolve(data)
-            }
-        });
-    })  
+    } catch (error) {
+        console.error(error)
+    }
 }
-const pingApi = () =>{
-    return new Promise((resolve,reject)=>{
-        const url = apiUrl + "/ping";
-        request({ url: url, json: true }, (error, res) => {
-            if (error) {
-                // cb("Unable to connect to coingecko api service!",undefined);
-                reject("Unable to connect to coingecko api service!");
-            }
-            try {
-                const data = res.body;
-                // cb(undefined,data)
-                resolve(data);
-            }catch(e){
-                reject("Unable to connect to coingecko api service!");
-            }
-        });
-    })
-       
+const pingApi = async () => {
+    const url = apiUrl + "/ping";
+    try {
+        return await axios.get(url)
+    } catch (error) {
+        console.error(error)
+    }
 }
-const priceOfCoins = (coins ,currency) =>{
-    return new Promise((resolve,reject)=>{
-        const url = apiUrl + "/simple/price?ids="+coins.join('%2C')+"&vs_currencies="+currency.join('%2C')+"&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true";
-        request({ url: url, json: true }, (error, res) => {
-            if (error) {
-                reject("Unable to connect to coingecko api service!");
-            }
-            else {
-                const data = res.body;
-                resolve(data)
-            }
-        });
-    })
-    
+const priceOfCoins = async (coins, currency) => {
+    const url = apiUrl + "/simple/price?ids=" + coins.join('%2C') + "&vs_currencies=" + currency.join('%2C') + "&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true";
+    try {
+        return await axios.get(url)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 module.exports = {
