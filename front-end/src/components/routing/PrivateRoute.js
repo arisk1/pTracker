b425418617e3f671eef6039984208754aa89bfd8
errikosg <errikosgf@hotmail.gr>
2021-05-13 20:26:31 +0300

    Hotfix for private route redirect on refresh

--- front-end/src/components/routing/PrivateRoute.js
+++ front-end/src/components/routing/PrivateRoute.js
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+    const authContext = useContext(AuthContext);    // important!
+
     return (
         <div>
             <Route
